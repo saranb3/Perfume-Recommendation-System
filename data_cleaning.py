f509d0d2240ebb3d78d40b14562db16087f4752c
saranb23 <148293225+saranb3@users.noreply.github.com>
2025-06-05 11:38:49 +0700

    Created a script to remove perfumes with no notes (cosine similarity will fail if included) and also added the new database as a new file called 'perfume_cleaned_database'

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np
+from pathlib import Path
 
 #loaded the dataset with encoding='iso-8859-1'
 df =  pd.read_csv('database/perfume_data.csv', encoding='iso-8859-1')
@@ -12,10 +13,25 @@ missing_notes = df['Notes'].isnull()
 missing_notes_table = df[missing_notes]
 print(missing_notes_table)
 
+# Modify the dataset to only include perfumes with notes  for cosine similarity
+perfume_cleaned_dataset = df.dropna(subset=['Notes']) 
+
+# Clean the notes column for excess space and capitalization consistency
+perfume_cleaned_dataset['Notes'] = perfume_cleaned_dataset['Notes'].str.strip().str.lower() 
 
 
+# Check how many rows remain
+print(f"\nAfter removing missing notes: {len(perfume_cleaned_dataset)} perfumes")
+print(f"Removed: {len(df) - len(perfume_cleaned_dataset)} perfumes")
 
+# Check if cleaning worked
+print("\nSample of cleaned notes:")
+for i in range(3):
+    print(f"'{perfume_cleaned_dataset['Notes'].iloc[i]}'")
     
+# Check for any remaining issues --> should equal zero
+print(f"\nAny remaining null notes? {perfume_cleaned_dataset['Notes'].isnull().sum()}")
 
-
-#Data cleaning: finding the rows without data
+# Create a csv file with the cleaned dataset and add it into the 'database' folder
+filepath = Path('database/perfume_cleaned_database.csv')
+perfume_cleaned_dataset.to_csv(filepath, index=False)
